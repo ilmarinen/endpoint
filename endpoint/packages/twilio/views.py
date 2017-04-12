f0cbdd209a3c602d87c49d2cc4523b02410ed497
Xavier Fernandes <pxferna@debian-vm-mac.home-local.net>
2017-04-12 00:50:30 -0700

    Get form Body instead of body

--- endpoint/packages/twilio/views.py
+++ endpoint/packages/twilio/views.py
@@ -11,6 +11,6 @@ bp = Blueprint('twilio_views', __name__, template_folder='templates')
 @bp.route('/sms', methods=['POST'])
 @permissions.accountsid_matches_token
 def receive_sms(**kwargs):
-    sms_body = request.form.get("body")
-    print(re.findall(urlmarker.WEB_URL_REGEX, sms_body))
+    sms_body = request.form.get("Body")
+    print re.findall(urlmarker.WEB_URL_REGEX, sms_body)
     return True
