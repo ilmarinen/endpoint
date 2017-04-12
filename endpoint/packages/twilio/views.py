52e60c95ccdb1460a8bddd8aa04b1236b3a9c171
Xavier Fernandes <pxferna@debian-vm-mac.home-local.net>
2017-04-12 00:43:43 -0700

    Fix typo

--- endpoint/packages/twilio/views.py
+++ endpoint/packages/twilio/views.py
@@ -10,7 +10,7 @@ bp = Blueprint('twilio_views', __name__, template_folder='templates')
 
 @bp.route('/sms', methods=['POST'])
 @permissions.accountsid_matches_token
-def receive_sms(self, **kwargs):
+def receive_sms(**kwargs):
     sms_body = request.form.get("body")
     print(re.findall(urlmarker.WEB_URL_REGEX, sms_body))
     return True
