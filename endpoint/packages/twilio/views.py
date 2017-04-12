d625ea83a50cff9140fcfcf91fcf3e7579ff8392
Xavier Fernandes <pxferna@debian-vm-mac.home-local.net>
2017-04-12 01:09:19 -0700

    Remove exclamation point

--- endpoint/packages/twilio/views.py
+++ endpoint/packages/twilio/views.py
@@ -12,5 +12,5 @@ bp = Blueprint('twilio_views', __name__, template_folder='templates')
 def receive_sms(**kwargs):
     sms_body = request.form.get("Body")
     print re.findall(urlmarker.WEB_URL_REGEX, sms_body)
-    xml_response = "<Response><Message>Thanks!</Message></Response>"
+    xml_response = "<Response><Message>Thanks</Message></Response>"
     return Response(xml_response, mimetype="text/xml")
