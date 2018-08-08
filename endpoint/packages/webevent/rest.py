04c72fd6a87fd02c6505b7d5148e1246b3deacd0
Xavier Fernandes <pxferna@xaviers-mbp.attlocal.net>
2018-08-08 01:10:43 -0700

    Remove unused imports

--- endpoint/packages/webevent/rest.py
+++ endpoint/packages/webevent/rest.py
@@ -1,9 +1,5 @@
 from endpoint.lib.http import RESTView, APIException
-from endpoint.lib import formatter
-from endpoint.packages.user import data_format
 from flask import Blueprint, request
-from flask_login import current_user, login_required, login_user, logout_user
-import permissions
 import manage
 
 
